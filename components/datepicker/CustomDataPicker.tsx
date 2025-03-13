5b9380445be33ad89949a3ba379e537dd9a794a2
sophia <thanhthuy16122001@gmail.com>
2025-03-13 23:13:57 +0700

    #Add recharge page

---
+++ components/datepicker/CustomDataPicker.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, { useState } from "react";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useTranslations } from "next-intl";
+
+interface CustomDatePickerProps {
+  value?: Date;
+  onChange?: (date: Date) => void;
+  className?: string;
+  title?: string;
+  placeHolder?: string;
+}
+
+const CustomDatePicker: React.FC<CustomDatePickerProps> = ({
+  value,
+  onChange,
+  className,
+  title,
+  placeHolder,
+}) => {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(value);
+  const t = useTranslations();
+
+  const handleDateChange = (date: Date | undefined) => {
+    setSelectedDate(date);
+    onChange?.(date as Date);
+  };
+
+  return (
+    <div>
+      <label>{title}</label>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            className={cn(
+              "w-[240px] justify-start text-left font-normal flex items-center gap-2",
+              !selectedDate && "text-muted-foreground",
+              className
+            )}
+          >
+            <CalendarIcon className="w-5 h-5" />
+            {selectedDate ? (
+              format(selectedDate, "PPP")
+            ) : (
+              <span>{placeHolder||t("global.pickADate")}</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={selectedDate}
+            onSelect={handleDateChange}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
+
+export default CustomDatePicker;
--- pages/recharge/Recharge.tsx
+++ pages/recharge/Recharge.tsx
@@ -1,8 +1,41 @@
-import React from "react";
+"use client";
+import { Card } from "@/components/ui/card";
+import { Select } from "@/components/ui/select";
+import { SelectContent, SelectItem } from "@radix-ui/react-select";
+import { useTranslations } from "next-intl";
+import React, { useState } from "react";
+import ShowQR from "./ShowQR";
+import History from "./History";
 
 // ----------------------------------------------------------------------
 
 const Recharge: React.FC = () => {
-  return <div>Recharge</div>;
+  const [state, setState] = useState({
+    rechargeTypes: [
+      {
+        label: "Momo",
+        value: "momo",
+      },
+      {
+        label: "Bank",
+        value: "bank",
+      },
+    ],
+  });
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mx-10">
+      <Card className="p-10 mb-8 col-span-1">
+        <ShowQR
+          value={
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+          }
+        />
+      </Card>
+
+      <Card className="p-10 mb-8 col-span-2">
+        <History rechargeTypes={state.rechargeTypes} />
+      </Card>
+    </div>
+  );
 };
 export default Recharge;
